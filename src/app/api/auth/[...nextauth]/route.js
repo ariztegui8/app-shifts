23e12156e2d472e4847226072c0807df207f405d
ariztegui8 <jorgeariztegui8@gmail.com>
2024-04-24 12:42:07 -0300

    add credentials

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,38 +3,49 @@ import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            
-            name: "Credentials",
-            
-            credentials: {
-              username: { label: "Username", type: "text", placeholder: "jsmith" },
-              password: { label: "Password", type: "password" }
+  providers: [
+    CredentialsProvider({
+
+      name: "Credentials",
+
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+
+      async authorize(credentials, req) {
+
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/login`,
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+            headers: {
+              "Content-Type": "application/json",
             },
-            async authorize(credentials, req) {
-              
-              const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-        
-              if (user) {
-                
-                return user
-              } else {
-               
-                return null
-        
-               
-              }
-            }
-          }),
 
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
+          }
+        )
 
-   
+        const user = await res.json()
+        console.log('user', user);
+
+        if(user.error) throw user
+        return user;
+
+      }
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+
 })
 
 export { handler as GET, handler as POST }
